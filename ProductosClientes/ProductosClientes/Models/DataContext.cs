39ab66a45930d77e2a0ba8e04d931b8c69a5f30c
Samuel Peña <samuel_pena@hotmail.es>
2019-03-09 10:31:04 -0400

    Entity Framework instalado

--- ProductosClientes/ProductosClientes/Models/DataContext.cs
+++ ProductosClientes/ProductosClientes/Models/DataContext.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace ProductosClientes.Models
 {
-    public class DataContext:DBContext
+    public class DataContext:DbContext
     {
 
     }
